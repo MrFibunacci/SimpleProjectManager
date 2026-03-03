d87ea4e8d8e2a28d9093dedf4b0cf184007ee8a1
MrFibunacci <mrfibunacci@gmail.com>
2026-03-03 12:36:43 +0100

    added tests

--- tests/Feature/View/Project/TasksTest.php
+++ tests/Feature/View/Project/TasksTest.php
@@ -7,13 +7,16 @@ use Tests\Feature\View\ViewTest;
 
 class TasksTest extends ViewTest
 {
+
+    private static string $viewName = 'project.tasks';
+
     protected function setUp(): void
     {
         parent::setUp();
 
         $this->testProject->tasks()->save(Task::factory()->make());
 
-        $this->setUpTestView('project.tasks', ['project' => $this->testProject, 'tasks' => $this->testProject->tasks]);
+        $this->setUpTestView(self::$viewName, ['project' => $this->testProject, 'tasks' => $this->testProject->tasks]);
     }
 
     public function test_title_is_correct(): void
@@ -39,9 +42,31 @@ class TasksTest extends ViewTest
         $this->testView->assertSeeInOrder([
             $task->id,
             $task->title,
-            $task->due_date,
-            //$task->status,
+            $task->due_date->diffForHumans(),
+            $task->status->name,
             'href', route('task.edit', $task), 'class="bi bi-pencil-square"'
+        ]);
+    }
+
+    public function test_table_row_is_yellow_if_task_is_due_tomorrow(): void
+    {
+        $task = $this->testProject->tasks()->first();
+        $task->due_date = now()->addDay();
+
+        $this->view(self::$viewName, ['project' => $this->testProject, 'tasks' => [$task]])->assertSeeInOrder([
+            'class="table-warning"',
+            $task->id,
+        ]);
+    }
+
+    public function test_table_row_is_red_if_task_was_due_in_past(): void
+    {
+        $task = $this->testProject->tasks()->first();
+        $task->due_date = now()->subDays(3);
+
+        $this->view(self::$viewName, ['project' => $this->testProject, 'tasks' => [$task]])->assertSeeInOrder([
+            'class="table-danger"',
+            $task->id,
         ]);
     }
 }
